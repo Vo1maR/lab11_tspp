a710c974a9d9b14aa3cbe232dfc2ddaf4ac5d19d
Vo1maR <94230738+Vo1maR@users.noreply.github.com>
2023-11-25 20:07:37 +0200

    Update TestCases.java

--- TestCases.java
+++ TestCases.java
@@ -3,29 +3,35 @@ public class TestCases {
     
     public static void main(String[] args) {
         int result_1 = testSendMessage();
-        String ispassed = "Failed";
-        if(result_1 == 1){
-        ispassed = "Passed";
+        String ispassed = "Passed";
+        bolean isAllPassed = true;
+        if(result_1 != 1){
+        ispassed = "Failed";
+        isAllPassed = false;
         }
         System.out.println("TC1: Очікуємий результат = 1, отриманий результат = " + result_1 + " ; " + ispassed);
-        ispassed = "Failed";
-        int result_2 = testSendMessageMoreThen350();
-        if(result_2 == -1){
         ispassed = "Passed";
+        int result_2 = testSendMessageMoreThen350();
+        if(result_2 != -1){
+        ispassed = "Failed";
+        isAllPassed = false;
         }
         System.out.println("TC2: Очікуємий результат = -1, отриманий результат = " + result_2 + " ; " + ispassed);
-        ispassed = "Failed";
-        int result_3 = testSendMessageEmptyText();
-        if(result_3 == -1){
         ispassed = "Passed";
+        int result_3 = testSendMessageEmptyText();
+        if(result_3 != -1){
+        ispassed = "Failed";
+        isAllPassed = false;
         }
         System.out.println("TC3: Очікуємий результат = -1, отриманий результат = " + result_3 + " ; " + ispassed);
         int result_4 = testSendMessageInvalidTime();
-        ispassed = "Failed";
-        if(result_4 == -2){
         ispassed = "Passed";
+        if(result_4 != -2){
+        ispassed = "Failed";
+        isAllPassed = false;
         }
         System.out.println("TC4: Очікуємий результат = -1, отриманий результат = " + result_4 + " ; " + ispassed);
+        if(isAllPassed) System.exit(0);
         System.exit(-1);
     }
 
