1870ca417d64268fa6cf305fc5bf459c6caab3b1
Vo1maR <94230738+Vo1maR@users.noreply.github.com>
2023-11-25 19:42:18 +0200

    Update TestCases.java

--- TestCases.java
+++ TestCases.java
@@ -13,7 +13,7 @@ public class TestCases {
         testSendMessageInvalidTime();
     }
 
-    public static void testSendMessage() {
+    public static int testSendMessage() {
         TestCases testCases = new TestCases();
         // Вызываем метод sendMessage с неверным временем
         int result = testCases.sendMessage("Не можу заснути, просто жесть", "testUser", new Date().getTime());
@@ -22,7 +22,7 @@ public class TestCases {
         return result;
     }
 
-    public static void testSendMessageMoreThen350() {
+    public static int testSendMessageMoreThen350() {
         TestCases testCases = new TestCases();
         // Вызываем метод sendMessage с неверным временем
         StringBuilder longText = new StringBuilder();
@@ -34,7 +34,7 @@ public class TestCases {
         return result;
     }
 
-    public static void testSendMessageEmptyText() {
+    public static int testSendMessageEmptyText() {
         TestCases testCases = new TestCases();
         // Вызываем метод sendMessage с неверным временем
         int result = testCases.sendMessage("", "testUser", new Date().getTime());
@@ -43,7 +43,7 @@ public class TestCases {
         return result;
     }
 
-    public static void testSendMessageInvalidTime() {
+    public static int testSendMessageInvalidTime() {
         TestCases testCases = new TestCases();
         // Вызываем метод sendMessage с неверным временем
         int result = testCases.sendMessage("Не можу заснути, просто жесть", "testUser", new Date().getTime() - 1);
@@ -55,12 +55,9 @@ public class TestCases {
     public int sendMessage(String textFieldText, String username, long currentTime) {
         // Реализация метода sendMessage без зависимостей от Android
         if (textFieldText.isEmpty() || textFieldText.length() > 350) return -1;
-        
-        // Создание объекта Message
-        Message newMessage = new Message(username, textFieldText);
 
         // Проверка соответствия времени сообщения переданному времени
-        if (newMessage.getMessageTime() != currentTime) return -2;
+        if (currentTime != new Date().getTime()) return -2;
 
         return 1;
     }
