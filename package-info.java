6f4e3697e8001d22c77da1d22e579e412867d7c7
Vo1maR <94230738+Vo1maR@users.noreply.github.com>
2023-11-25 18:53:00 +0200

    Update package-info.java

--- package-info.java
+++ package-info.java
@@ -1,11 +1,13 @@
 /**
  * This package contains classes related to message handling.
  * <p>
- * The main class in this package is {@link com.example.myapplication.models.Message}, which represents a message in the system.
- * It includes information such as the sender's username, the text content of the message, and the timestamp.
+ * The main class in this package is {@link com.example.myapplication.models.Message},
+ * which represents a message in the system. It includes information such as the
+ * sender's username, the text content of the message, and the timestamp.
  * </p>
  * <p>
- * Other classes in this package may provide additional functionality related to message handling.
+ * Other classes in this package may provide additional functionality related to
+ * message handling.
  * </p>
  */
 package com.example.myapplication.models;
