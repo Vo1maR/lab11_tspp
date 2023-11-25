46dc3aa49b0ca14fbb93accd520097c7cab7b243
Vo1maR <94230738+Vo1maR@users.noreply.github.com>
2023-11-25 18:38:56 +0200

    Update Message.java

--- Message.java
+++ Message.java
@@ -2,40 +2,82 @@ package com.example.myapplication.models;
 
 import java.util.Date;
 
-public class Message {
+/**
+ * Represents a message in the system.
+ */
+public final class Message {
     private String userName;
     private String textMessage;
     private long messageTime;
 
-    public Message(){}
-    public Message(String userName, String textMessage) {
+    /**
+     * Default constructor.
+     */
+    public Message() {}
+
+    /**
+     * Creates a new message with the given username and text message.
+     *
+     * @param userName     The username of the sender.
+     * @param textMessage  The text content of the message.
+     */
+    public Message(final String userName, final String textMessage) {
         this.userName = userName;
         this.textMessage = textMessage;
-
         this.messageTime = new Date().getTime();
     }
 
+    /**
+     * Gets the username of the sender.
+     *
+     * @return The username.
+     */
     public String getUserName() {
         return userName;
     }
 
-    public void setUserName(String userName) {
-        this.userName = userName;
+    /**
+     * Sets the username of the sender.
+     *
+     * @param newUserName The new username.
+     */
+    public void setUserName(final String newUserName) {
+        this.userName = newUserName;
     }
 
+    /**
+     * Gets the text content of the message.
+     *
+     * @return The text content.
+     */
     public String getTextMessage() {
         return textMessage;
     }
 
-    public void setTextMessage(String textMessage) {
-        this.textMessage = textMessage;
+    /**
+     * Sets the text content of the message.
+     *
+     * @param newTextMessage The new text content.
+     */
+    public void setTextMessage(final String newTextMessage) {
+        this.textMessage = newTextMessage;
     }
 
+    /**
+     * Gets the timestamp of the message.
+     *
+     * @return The timestamp.
+     */
     public long getMessageTime() {
         return messageTime;
     }
 
-    public void setMessageTime(long messageTime) {
-        this.messageTime = messageTime;
+    /**
+     * Sets the timestamp of the message.
+     *
+     * @param newMessageTime The new timestamp.
+     */
+    public void setMessageTime(final long newMessageTime) {
+        this.messageTime = newMessageTime;
     }
 }
